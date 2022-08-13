92ba56c9c09e1e18b34c0aedd013cabdcfb6a6bc
CsAdi <oradam86@gmail.com>
2022-08-13 13:42:05 +0200

    1:37:17 { Header ( Search bar, Upcoming flights) }

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:booking/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
             child: Column(
               children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -24,9 +27,7 @@ class HomeScreen extends StatelessWidget {
                           "Good Morning",
                           style: Styles.headLineStyle3,
                         ),
-                        const SizedBox(
-                          height: 5.0,
-                        ),
+                        const Gap(5),
                         Text(
                           "Book Tickets",
                           style: Styles.headLineStyle1,
@@ -44,6 +45,47 @@ class HomeScreen extends StatelessWidget {
                         ),
                       ),
                     ),
+                  ],
+                ),
+                const Gap(40),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: const Color(0xFFF4F6F0),
+                  ),
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      ),
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Upcoming flights",
+                      style: Styles.headLineStyle2,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        print("You are tapped");
+                      },
+                      child: Text(
+                        "View all",
+                        style: Styles.textStyle.copyWith(
+                          color: Styles.primaryColor,
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ],
