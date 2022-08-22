c0a33db9d49da823cedd0355db9b1f31e70a2f1f
CsAdi <oradam86@gmail.com>
2022-08-22 17:24:47 +0200

    3:16:25 { TicketView | AppInfoList[hotelList] | HomeScreen }

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -5,7 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class TicketView extends StatelessWidget {
-  const TicketView({Key? key}) : super(key: key);
+  final Map<String, dynamic> ticket;
+
+  const TicketView({Key? key, required this.ticket}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +35,7 @@ class TicketView extends StatelessWidget {
                   Row(
                     children: [
                       Text(
-                        "NYC",
+                        "${ticket['from']['code']}",
                         style: Styles.headLineStyle3.copyWith(
                           color: Colors.white,
                         ),
@@ -77,10 +79,10 @@ class TicketView extends StatelessWidget {
                         ),
                       ), // const Spacer(),
 
-                      ThickContainer(),
+                      const ThickContainer(),
                       Expanded(child: Container()), // const Spacer(),
                       Text(
-                        "LDN",
+                        "${ticket['to']['code']}",
                         style: Styles.headLineStyle3.copyWith(
                           color: Colors.white,
                         ),
@@ -94,18 +96,18 @@ class TicketView extends StatelessWidget {
                       SizedBox(
                         width: 100.0,
                         child: Text(
-                          "New-York",
+                          "${ticket['from']['name']}",
                           style: Styles.headLineStyle4.copyWith(color: Colors.white),
                         ),
                       ),
                       Text(
-                        "8H 30M",
+                        "${ticket['flying_time']}",
                         style: Styles.headLineStyle4.copyWith(color: Colors.white),
                       ),
                       SizedBox(
                         width: 100.0,
                         child: Text(
-                          "London",
+                          "${ticket['to']['name']}",
                           textAlign: TextAlign.end,
                           style: Styles.headLineStyle4.copyWith(color: Colors.white),
                         ),
@@ -192,7 +194,7 @@ class TicketView extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            "1 MAY",
+                            "${ticket['date']}",
                             style: Styles.headLineStyle3.copyWith(color: Colors.white),
                           ),
                           const Gap(5),
@@ -206,7 +208,7 @@ class TicketView extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
                           Text(
-                            "08:00 AM",
+                            "${ticket['departure_time']}",
                             style: Styles.headLineStyle3.copyWith(color: Colors.white),
                           ),
                           const Gap(5),
@@ -220,7 +222,7 @@ class TicketView extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           Text(
-                            "23",
+                            "${ticket['number']}",
                             style: Styles.headLineStyle3.copyWith(color: Colors.white),
                           ),
                           const Gap(5),
--- lib/utils/app_info_list.dart
+++ lib/utils/app_info_list.dart
@@ -18,3 +18,48 @@ List<Map<String, dynamic>> hotelList = [
     "price": 68,
   },
 ];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code': "NYC",
+      'name': "New York",
+    },
+    'to': {
+      'code': "LDN",
+      'name': "London",
+    },
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    "number": 23,
+  },
+  {
+    'from': {
+      'code': "DK",
+      'name': "Dhaka",
+    },
+    'to': {
+      'code': "SH",
+      'name': "Shanghai",
+    },
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time': "09:00 AM",
+    "number": 45,
+  },
+  {
+    'from': {
+      'code': "WDC",
+      'name': "Washington DC",
+    },
+    'to': {
+      'code': "MCT",
+      'name': "Man City",
+    },
+    'flying_time': '10H 30M',
+    'date': "16 JUNE",
+    'departure_time': "06:00 PM",
+    "number": 52,
+  },
+];
