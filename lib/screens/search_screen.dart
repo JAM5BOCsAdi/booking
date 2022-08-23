dbd46d408401df6e614de31d25af4f2ceb14debe
CsAdi <oradam86@gmail.com>
2022-08-23 18:20:50 +0200

    4:25:43 {FINISHED the SearchScreen[2] }

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -87,6 +87,143 @@ class SearchScreen extends StatelessWidget {
           ),
           Gap(AppLayout.getHeight(40.0)),
           const AppDoubleTextWidget(bigText: "Upcoming Flights", smallText: "View All"),
+          Gap(AppLayout.getHeight(15.0)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: AppLayout.getHeight(400.0),
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15.0), vertical: AppLayout.getWidth(15.0)),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(12.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      blurRadius: 1.0,
+                      spreadRadius: 1.0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(200.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(AppLayout.getHeight(12.0)),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/sit.jpg"),
+                        ),
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12.0)),
+                    Text(
+                      "20% discount on the early booking of this flight. Don't miss out this chance ",
+                      style: Styles.headLineStyle2,
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: AppLayout.getHeight(190.0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF3AB8B8),
+                          borderRadius: BorderRadius.circular(
+                            AppLayout.getHeight(18.0),
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: AppLayout.getHeight(15.0),
+                          horizontal: AppLayout.getWidth(15.0),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Discount\nfor survey",
+                              style: Styles.headLineStyle2.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Gap(AppLayout.getHeight(10.0)),
+                            Text(
+                              "Take the survey about our services and get discount",
+                              style: Styles.headLineStyle2.copyWith(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.0,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: -45.0,
+                        top: -40.0,
+                        child: Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(30.0)),
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 18.0,
+                              color: const Color(0xFF189999),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Gap(AppLayout.getHeight(10.0)),
+                  Container(
+                    width: size.width * 0.44,
+                    height: AppLayout.getHeight(200.0),
+                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(15.0), horizontal: AppLayout.getWidth(15.0)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(AppLayout.getHeight(18.0)),
+                      color: const Color(0xFFEC6545),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Take Love",
+                          style: Styles.headLineStyle2.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        Gap(AppLayout.getHeight(10.0)),
+                        RichText(
+                          text: const TextSpan(
+                            children: [
+                              TextSpan(
+                                text: '😍',
+                                style: TextStyle(fontSize: 30.0),
+                              ),
+                              TextSpan(
+                                text: '🥰',
+                                style: TextStyle(fontSize: 40.0),
+                              ),
+                              TextSpan(
+                                text: '😘',
+                                style: TextStyle(fontSize: 30.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
