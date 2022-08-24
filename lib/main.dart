849a673079e954712421f931f3f7379426e50d43
CsAdi <oradam86@gmail.com>
2022-08-24 14:20:50 +0200

    5:48:37 { FINISHED the TicketScreen | Added Barcode package }

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My Tickets App',
+      title: 'Booking App',
       theme: ThemeData(
         primaryColor: primary,
       ),
